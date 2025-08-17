0b306c63fd52a855e45c518eef0bc3ea648af76c
ViniRaiz7 <lendalv444@gmail.com>
2025-08-17 17:26:46 -0300

    Update Main.java

--- vini.java
+++ vini.java
@@ -1,14 +1,14 @@
 public class vini {
     public static void main(String[] args) {
-        // Instanciação de dois objetos da classe ProjetoLivraria
-        ProjetoLivraria livro1 = new ProjetoLivraria("Dom Casmurro", "Machado de Assis", 50.0);
-        ProjetoLivraria livro2 = new ProjetoLivraria("O Hobbit", "J.R.R. Tolkien", 70.0);
+        
+        ProjetoLivraria livro1 = new ProjetoLivraria("Java Para iniciantes", "ViniRaiz7", 50.0);
+        ProjetoLivraria livro2 = new ProjetoLivraria("php Para iniciantes", "ViniRaiz7", 70.0);
 
-        // Alteração dos atributos através dos objetos
-        livro1.setPreco(45.0); // altera o preço do livro1
-        livro2.setAutor("Tolkien"); // altera o autor do livro2
+        
+        livro1.setPreco(45.0);
+        livro2.setAutor("ViniRaiz7");
 
-        // Impressão dos atributos na tela
+        
         System.out.println("Livro 1:");
         System.out.println("Título: " + livro1.getTitulo());
         System.out.println("Autor: " + livro1.getAutor());
@@ -19,9 +19,9 @@ public class vini {
         System.out.println("Autor: " + livro2.getAutor());
         System.out.println("Preço: " + livro2.getPreco());
 
-        // Invocação dos métodos da classe
-        livro1.aplicarDesconto(10); // Aplica 10% de desconto
-        livro2.aplicarDesconto(5);  // Aplica 5% de desconto
+    
+        livro1.aplicarDesconto(10);
+        livro2.aplicarDesconto(5);
 
         System.out.println("\nApós desconto:");
         System.out.println("Livro 1 - Preço: " + livro1.getPreco());
